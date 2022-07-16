ea7db3dadec6ef19934d9c8e23a7e4ee3614c178
AliakseiSafonau <pharmaceftssma@gmail.com>
2022-07-16 20:13:05 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App"></div>
+    <header class="header">
+        <div class="logo">
+            <img src="../travel/img/Vector.png" alt="logo"></img>
+            <span class="logo_title">Travel Portal</span>  
+        </div>
+        <nav class="navigation">
+            <span class="list_title mr0">How It Works</span>
+            <span class="list_title">Plan Your Trip</span>
+            <span class="list_title">Destinations</span>
+            <span class="list_title">Travel Stories</span>
+        </nav>
+        <button class="login">Login</button>
+    </header>
   );
 }
 
