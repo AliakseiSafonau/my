b9eeb1c2b3e33bdb7a4f4526e14f1db3c4e1a78d
AliakseiSafonau <pharmaceftssma@gmail.com>
2022-07-16 21:53:48 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
+import './App.css';
+import Header from './Header';
 
 function App() {
-  return (
-    <header class="header">
-        <div class="logo">
-            <img src="../travel/img/Vector.png" alt="logo"></img>
-            <span class="logo_title">Travel Portal</span>  
-        </div>
-        <nav class="navigation">
-            <span class="list_title mr0">How It Works</span>
-            <span class="list_title">Plan Your Trip</span>
-            <span class="list_title">Destinations</span>
-            <span class="list_title">Travel Stories</span>
-        </nav>
-        <button class="login">Login</button>
-    </header>
-  );
-}
+  return (<Header />);}
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Vector from'./Img/Vector.png';
+
+class Header extends React.Component {
+    render () {
+        return (
+            <header className="header">
+                <div className="logo">
+                    <img src={Vector} alt="logo"></img>
+                    <span className="logo_title">Travel Portal</span>  
+                </div>
+                <nav className="navigation">
+                    <span className="list_title mr0">How It Works</span>
+                    <span className="list_title">Plan Your Trip</span>
+                    <span className="list_title">Destinations</span>
+                    <span className="list_title">Travel Stories</span>
+                </nav>
+                <button className="login">Login</button>
+            </header>
+        );
+    }
+}
+
+export default Header;
